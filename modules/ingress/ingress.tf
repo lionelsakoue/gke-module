031bedf19c42856be1259ea98aa1ea7d79172244
lionelsakoue <sakouepierre@gmail.com>
2025-09-27 23:55:46 -0400

    updated ingress.tf

--- modules/ingress/ingress.tf
+++ modules/ingress/ingress.tf
@@ -4,4 +4,17 @@ resource "helm_release" "istio_ingress" {
   chart      = "gateway"
   version    = var.istio_version
   namespace  = var.namespace
+
+  values = [
+    yamlencode({
+      global = {
+        proxy = {
+          image = "gcr.io/istio-release/proxyv2:1.24.2"
+        }
+      }
+    })
+  ]
+
+  timeout = 600
+  wait    = true
 }
